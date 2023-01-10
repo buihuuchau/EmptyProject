11123ca60e5fed030ae0257f32e25520109c4342
buihuuchau <buihuuchau99@gmail.com>
2023-01-10 13:23:28 +0700

    Enum, custompagelogin, lang, 404 page, view pagination

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/custompagelogin', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
 
 //  Start The Email Verification Notice
